d479d20d807aefc178382681db3f64e37a005221
pabloski300 <32078402+pabloski300@users.noreply.github.com>
2018-03-19 19:14:13 +0100

    Código funcionando y comentado

--- planifica/src/planifica/Planifica.java
+++ planifica/src/planifica/Planifica.java
@@ -5,11 +5,6 @@
  */
 package planifica;
 
-/**
- *
- * @author p.rodriguezvic
- */
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,6 +18,10 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ *
+ * @author pablo
+ */
 public class Planifica {
 
     File entrada, salida;
@@ -30,6 +29,7 @@ public class Planifica {
     List<Reunion> reuniones = new ArrayList<>();
     List<Integer> solucion = new ArrayList<>();
     Scanner teclado = new Scanner(System.in);
+    boolean archivoDeSalida = true;
     
     /**
      * @param args the command line arguments
@@ -37,35 +37,76 @@ public class Planifica {
     public static void main(String[] args) {
         new Planifica().exec(args);  
     }
+    
+    /**
+     * Este es el algoritmo voraz, consiste un un bucle que elige un candidato, 
+     * lo saca de la lista de candidatos, actualiza las horas del resto de 
+     * candidatos restandoles la hora de fin del candidato y elimina los candidatos
+     * que ya no sea posible usar por las condiciones del problema, por último 
+     * introduce el candidato en la solucion.
+     */
 
     public void AlgoritmoVoraz() {
-        int i = 0;
         while(!reuniones.isEmpty()){
-            Reunion reunion = reuniones.get(i);
-            solucion.add(reunion.getID());
-            reuniones.remove(reunion);
+            
+            Reunion candidato = SeleccionarCandidato();     //Funcion de seleccion.
+            
+            reuniones.remove(candidato);                    //Eliminacion del candidato de la lista
+            
+            for(int i=0; i<reuniones.size(); i++){          //Bucle de actualizacion de candidatos
+                reuniones.get(i).ActualizarHoras(candidato);
+            }
+            
+            int i = 0;
+            while (i < reuniones.size()) {                  //Bucle de eliminacion de los cnadidatos no posibles
+                if(reuniones.get(i).getHoraInicio() < 0){
+                   reuniones.remove(reuniones.get(i));
+                   i--;
+                }
+                i++;
+            }
+            
+            solucion.add(candidato.getID());                //Introduccion del candidato en la solución
+            
         }
         
     }
     
+    /**
+     * Este es el método encargado de las lecturas y escrituras de ficheros 
+     * y de las entradas por teclado y salidas por pantalla.
+     * @param args
+     */
     public void exec(String[] args){
         
-        if(args.length == 0){
+        if(args.length > 1){ //Se comprueba si el archivo de salida ya existe y se pideo o elegir uno nuevo o no elegir ninguno
+            salida = new File(args[1]+".txt");
+            if(salida.exists()){
+                System.out.println("el archivo de salida ya existe, por favor indique otro nombre para el archivo, "
+                        + "si no quiere elegir un archivo pulse la tecla intro, el resultado será impreso por pantalla");
+                args[1] = teclado.nextLine();
+                if(args[1].isEmpty()){
+                    archivoDeSalida = false;
+                }
+            }    
+        }
+        
+        if(args.length == 0){ //Se comprueba si se ha elegido un archivo de entrada, en caso negativo se piden datos por teclado, en caso positivo se lee el archivo
             System.out.println("No se han detectado fichero de entrada, por favor"
                     + "introduce los datos por teclado con el siguiente formato:[numero de tareas, "
                     + "inicio de reunion espacio en blanco final de reunion]");
             
             System.out.println("Introduce el numero de tareas");
-            numeroDatos = Integer.parseInt(teclado.next());
+            numeroDatos = Integer.parseInt(teclado.nextLine());
             
             for(int i=0; i<numeroDatos; i++){
-                System.out.println("Introduce el dato "+(i+1));
+                System.out.println("\nIntroduce el dato "+(i+1));
                 
-                System.out.println("Introduce la hora de entrada");
-                int inicio = Integer.parseInt(teclado.next());
+                System.out.println("\nIntroduce la hora de entrada");
+                int inicio = Integer.parseInt(teclado.nextLine());
                 
-                System.out.println("Introduce la hora de salida");
-                int fin = Integer.parseInt(teclado.next());
+                System.out.println("\nIntroduce la hora de salida");
+                int fin = Integer.parseInt(teclado.nextLine());
                 
                 Reunion reunion = new Reunion(inicio,fin,i);
                 
@@ -75,7 +116,7 @@ public class Planifica {
             System.out.println(args[0]);
             FileReader f = null;
             try {
-                entrada = new File(args[0]);
+                entrada = new File(args[0]+".txt");
                 f = new FileReader(entrada);
                 BufferedReader b = new BufferedReader(f);
                 
@@ -104,24 +145,21 @@ public class Planifica {
                 }
             }
             
-            //Para debuggear
-            /*for(int i = 0; i< numeroDatos; i++){
-                
-                System.out.println(reuniones.get(i).getHoraInicio()+" "+ reuniones.get(i).getHoraFin());
-                 
-            }*/
-            
         }
         
-        AlgoritmoVoraz();
+        AlgoritmoVoraz(); //Llamada al algoritmo voraz
         
-        if(args.length < 2){
+        if(args.length < 2 || !archivoDeSalida){ //Se comprueba si se ha elegido un archivo de salida, en caso negativo se imprime por pantalla en caso positivo se escribe en el fichero
+            
+            System.out.print("La solución es: ");
+            
             for(int i = 0; i< solucion.size();i++){
-                System.out.println(solucion.get(i));
+                System.out.print(solucion.get(i)+", ");
             }
-        }else{
+        }else if(args.length >=2 && archivoDeSalida){
             System.out.println(args[1]);
-            salida = new File(args[1]);
+            salida = new File(args[1]+".txt");
+            
             FileWriter f = null;
             try {
                 f = new FileWriter(salida);
@@ -142,5 +180,23 @@ public class Planifica {
             }
 
         }
+    }
+    
+    /**
+     * Esta funcion se encarga de elegir el mejor condidato de la lista de candidatos
+     * para ello se escoge el candidato con menor hora de finalizacion.
+     */
+    private Reunion SeleccionarCandidato() {
+        Reunion candidato = new Reunion(0,25,0);                        //Creación de un candidato para las primeras comparaciones
+        
+        for (int i = 0; i < reuniones.size(); i++) {                    
+            
+            if(reuniones.get(i).getHoraFin()<= candidato.getHoraFin()){ //Condicion para elegir el candidato
+                    candidato = reuniones.get(i);
+            }
+            
+        }
+        
+        return candidato;
     }
 }
--- planifica/src/planifica/Reunion.java
+++ planifica/src/planifica/Reunion.java
@@ -13,6 +13,7 @@ class Reunion {
     private int horaInicio;
     private int horaFin;
     private int ID;
+    private int duracion;
 
     public int getHoraInicio() {
         return horaInicio;
@@ -38,10 +39,27 @@ class Reunion {
         this.ID = ID;
     }
 
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
     public Reunion(int horaInicio, int horaFin, int ID) {
         this.horaInicio = horaInicio;
         this.horaFin = horaFin;
         this.ID = ID;
+        if(horaFin != 0){
+            this.duracion = horaFin - horaInicio;
+        }else if(horaFin == 0){
+            this.duracion = 24 - horaInicio;
+        }
     }
     
+    public void ActualizarHoras(Reunion candidato){
+        this.horaInicio -= candidato.getHoraFin();
+        this.horaFin -= candidato.getHoraFin();
+    }
 }
